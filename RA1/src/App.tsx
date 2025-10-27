5e93f2fccc8bd7284a4fa9a57f2abfd99ac662ff
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-10-27 18:58:35 +0100

    feat: add nav

--- RA1/src/App.tsx
+++ RA1/src/App.tsx
@@ -3,11 +3,45 @@ import './App.css';
 import MovieCard from './MovieCard';
 import { moviesData} from "./moviesData";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+
 function App() {
 
   return (
     <>
+    <nav style={{ backgroundColor: "#2C3545", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0.8rem 2rem" }}>
+    
+          <ul style={{
+          display: "flex",
+          justifyContent: "space-between",
+          listStyle: "none",
+          gap: "2rem",
+          alignItems: "center"
+
+        }}>
+          <li> <FontAwesomeIcon className="top-nav-icon" icon={faBars}  style={{
+            height: "24px", width: "24px", display: "flex", alignItems: "center", padding: "0.4rem", borderRadius: "8px",
+          }} /></li>
+          <li style={{ fontSize: "1.4rem", fontWeight: "600" }}>ITBFlix</li>
+          <li><div style={{ backgroundColor: "#374151", borderRadius: "15px", padding: "0.3rem 1.5rem" }}>🔎 Search Movies & TV</div></li>
+        </ul>
+        <ul style={{
+          display: "flex",
+          justifyContent: "space-between",
+          listStyle: "none",
+          gap: "2rem",
+          alignItems: "center"
+        }}>
+          <li><a href="">Films</a></li>
+          <li><a href="">Discover</a></li>
+          <li><a href="">Groups</a></li>
+        </ul>
+        </nav>
+
     <div className="movie-grid">
+      {}
     {moviesData.map((movie)=> {
       return(
       <MovieCard
@@ -17,6 +51,8 @@ function App() {
       />
       )
     })}
+          {}
+
     </div>
     </>
   )
