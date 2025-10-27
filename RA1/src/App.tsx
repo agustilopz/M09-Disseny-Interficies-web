a14155d33dbab3f2d8596adfda4818e66a351bca
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-10-27 20:10:03 +0100

    chore: update head info

--- RA1/src/App.tsx
+++ RA1/src/App.tsx
@@ -6,15 +6,11 @@ import { moviesData} from "./moviesData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-
 function App() {
 
   return (
     <>
-
-
     <nav style={{ backgroundColor: "#2C3545", display: "flex", justifyContent: "space-between", alignItems: "center", padding: "0.8rem 2rem" }}>
-    
           <ul style={{
           display: "flex",
           justifyContent: "space-between",
@@ -27,7 +23,7 @@ function App() {
             height: "24px", width: "24px", display: "flex", alignItems: "center", padding: "0.4rem", borderRadius: "8px",
           }} /></li>
           <li style={{ fontSize: "1.4rem", fontWeight: "600" }}>ITBFlix</li>
-          <li><div style={{ backgroundColor: "#374151", borderRadius: "15px", padding: "0.3rem 1.5rem" }}>🔎 Search Movies & TV</div></li>
+          <li><div style={{ backgroundColor: "#374151", borderRadius: "15px", padding: "0.3rem 1.5rem", width: "50vh", textAlign: "left" }}>🔎 Search Movies & TV</div></li>
         </ul>
         <ul style={{
           display: "flex",
@@ -38,12 +34,12 @@ function App() {
         }}>
           <li><a href="">Films</a></li>
           <li><a href="">Discover</a></li>
-          <li><a href="">Groups</a></li>
+          <li><a href="">Coming Soon</a></li>
+          <li><a href="">Lists</a></li>
         </ul>
         </nav>
 
     <div className="movie-grid">
-      {}
     {moviesData.map((movie)=> {
       return(
       <MovieCard
@@ -54,13 +50,9 @@ function App() {
       />
       )
     })}
-          {}
-
     </div>
-
     </>
   )
-  
 }
 
 export default App
--- RA1/src/MovieCard.tsx
+++ RA1/src/MovieCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App';
 
-
 interface MovieCardProps {
   image: string;
   title: string;
@@ -14,7 +13,7 @@ const MovieCard: React.FC<MovieCardProps> = ({image, title, year, duration}) => 
     return (
     <>
     <div className="movie-card">
-    <img src={image} alt="" />
+    <img src={image} alt={title} />
     <h3>{title}</h3>
     <p className='movie-card-info'>
     <span className='movie-card-info-year'>{year}</span> &nbsp;·&nbsp; {duration}
