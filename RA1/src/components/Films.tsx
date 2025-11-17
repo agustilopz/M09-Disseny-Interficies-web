a4ec526e0e1486432dfb5b85ef3464fa77c62b5b
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-11-17 19:53:45 +0100

    feat:add nav

---
+++ RA1/src/components/Films.tsx
@@ -0,0 +1,31 @@
+import '../App.css';
+import MovieCard from '../components/MovieCard';
+
+import { moviesData} from "../moviesData";
+
+const Films: React.FC = () => {
+
+
+  return (
+    <>
+
+    <h4 className="block pt-8 pl-10 font-medium text-2xl text-left"><span className="hover:text-gray-400 inline-block cursor-pointer">Top Rated Movies</span></h4>
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 p-8">
+    
+    {moviesData.map((movie)=> {
+      return(
+      <MovieCard
+      image={movie.poster_path}
+      title={movie.original_title}
+      year={movie.year}
+      duration={movie.runtime}
+      />
+      )
+    })}
+    </div>
+
+    </>
+  )
+}
+
+export default Films;
